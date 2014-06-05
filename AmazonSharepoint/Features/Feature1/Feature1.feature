aaf9fdb8bea5810253d3740282ebd4a0535d1fdc
Toomas Tahves <toomastahves@hotmail.com>
2014-06-06 00:27:54 +0300

    init

---
+++ AmazonSharepoint/Features/Feature1/Feature1.feature
@@ -0,0 +1,12 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="80845ef6-d2bf-45a4-83bc-5f6e4541246b" description="SharePoint App Feature" featureId="80845ef6-d2bf-45a4-83bc-5f6e4541246b" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="AmazonSharepoint Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="1a1a5da1-fd72-4c49-92d1-cdd3acdc4870" />
+    <projectItemReference itemId="4d66e926-74c1-4ac1-92c6-3c323c69aeff" />
+    <projectItemReference itemId="13213303-55cc-44c5-bf4e-91035e593406" />
+    <projectItemReference itemId="5452e045-59b4-4da6-916d-29c74af76ad0" />
+    <projectItemReference itemId="65eb8c51-0b5a-4893-a1a9-916677a55bda" />
+    <projectItemReference itemId="312fbcb3-4a12-4096-b89e-2b15625f6b6b" />
+    <projectItemReference itemId="da8f6b63-ebd7-4b48-89a5-603552110953" />
+  </projectItems>
+</feature>
---
+++ AmazonSharepoint/Scripts/App.js
@@ -0,0 +1,125 @@
+﻿
+$(document).ready(function () {
+    console.log("Ready for action");
+    //createList();
+    //addField();
+    //createListItem();
+    //readItems();
+    $("input[id$='ButtonCreateList']").click(createList);
+    $("input[id$='ButtonAddFields']").click(addField);
+    $("input[id$='ButtonAddItem']").click(createListItem);
+    $("input[id$='ButtonReadItems']").click(readItems);
+});
+
+// Script for creating internal list.
+function createList() {
+    var clientContext = SP.ClientContext.get_current();
+    var oWebsite = clientContext.get_web();
+    var listCreationInfo = new SP.ListCreationInformation();
+    listCreationInfo.set_title("My Favorite Books");
+    listCreationInfo.set_templateType(SP.ListTemplateType.genericList);
+    var oList = oWebsite.get_lists().add(listCreationInfo);
+    clientContext.load(oList);
+    clientContext.executeQueryAsync(
+      successHandler,
+       errorHandler
+    );
+    function successHandler() {
+        $("#infoLabel").text("<My Favorite Books> list has been created!");
+        console.log("List successfully created.");
+    }
+    function errorHandler() {
+        $("#infoLabel").text("<My Favorite Books> list already exist!");
+        console.log("List already created, not creating new one.");
+    }
+}
+
+// Script for adding field to list.
+function addField() {
+    var clientContext = SP.ClientContext.get_current();
+    var oWebsite = clientContext.get_web();
+    var oList = oWebsite.get_lists().getByTitle("My Favorite Books");
+    var oField = oList.get_fields().addFieldAsXml(
+        "<Field DisplayName='Price' Type='Number' />",
+        true,
+        SP.AddFieldOptions.defaultValue
+    );
+    var fieldNumber = clientContext.castTo(oField, SP.FieldNumber);
+    fieldNumber.set_maximumValue(100);
+    fieldNumber.set_minimumValue(1);
+    fieldNumber.update();
+    clientContext.load(oField);
+    clientContext.executeQueryAsync(
+      successHandler,
+       errorHandler
+    );
+    function successHandler() {
+        $("#infoLabel").text("New field added to list!");
+        console.log("Field successfully added.");
+    }
+    function errorHandler() {
+        $("#infoLabel").text("Field not added to list!");
+        console.log("Field has not been added.");
+    }
+}
+
+// Script for adding items to list.
+function createListItem() {
+    var clientContext = SP.ClientContext.get_current();
+    var oWebsite = clientContext.get_web();
+    var oList = oWebsite.get_lists().getByTitle("My Favorite Books");
+    var itemCreateInfo = new SP.ListItemCreationInformation();
+    var oListItem = oList.addItem(itemCreateInfo);
+    oListItem.set_item("Title", "My New Item!");
+    oListItem.set_item("MyField", 1);
+    oListItem.update();
+    clientContext.load(oListItem);
+    clientContext.executeQueryAsync(
+      successHandler,
+       errorHandler
+    );
+    function successHandler() {
+        $("#infoLabel").text("New item added to list!");
+        console.log("Item successfully added.");
+    }
+    function errorHandler() {
+        $("#infoLabel").text("Problem while adding item.");
+        console.log("Item not added: " + arguments[1].get_message());
+    }
+}
+
+// Script for reading list items.
+function readItems() {
+    var clientContext = SP.ClientContext.get_current();
+    var oWebsite = clientContext.get_web();
+    var oList = oWebsite.get_lists().getByTitle("My Favorite Books");
+    var camlQuery = new SP.CamlQuery();
+    camlQuery.set_viewXml(
+        '<View><Query><Where><Geq><FieldRef Name=\'ID\'/>' +
+        '<Value Type=\'Number\'>1</Value></Geq></Where></Query>' +
+        '<RowLimit>10</RowLimit></View>'
+    );
+    var collListItem = oList.getItems(camlQuery);
+    clientContext.load(collListItem);
+    clientContext.executeQueryAsync(
+      successHandler,
+       errorHandler
+    );
+    function successHandler() {
+        var listItemEnumerator = collListItem.getEnumerator();
+        var listItemInfo = "";
+        while (listItemEnumerator.moveNext()) {
+            var oListItem = listItemEnumerator.get_current();
+            listItemInfo += "ID: " + oListItem.get_id() + "<br/>" +
+                "Title: " + oListItem.get_item("Title") + "<br/>" +
+                "MyField: " + oListItem.get_item("MyField") + "<br/>";
+        }
+        $("#infoLabel").text("Items read from list:" + listItemInfo);
+        console.log("Items successfully read: " + listItemInfo);
+    }
+    function errorHandler() {
+        $("#infoLabel").text("Problem while reading items.");
+        console.log("Problem while reading items: " + arguments[1].get_message());
+    }
+}
+
